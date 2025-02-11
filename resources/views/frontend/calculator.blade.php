906cf27432206d85738cfcf32459cd6ea47d573d
kj6560 <kj6560@gmail.com>
2025-02-11 16:15:59 +0530

    abc

--- resources/views/frontend/calculator.blade.php
+++ resources/views/frontend/calculator.blade.php
@@ -7,6 +7,28 @@ $banner = !empty($page_data->page_banner)
 : asset("brahmani_frontend_assets/images/bg/titlebar-img.jpg");
 
 @endphp
+<style>
+    /* Centering container */
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    /* Enhancing the table */
+    table {
+        border-radius: 10px;
+        overflow: hidden;
+        margin-top: 50px;
+    }
+
+    th,
+    td {
+        padding: 15px;
+        font-size: 18px;
+    }
+</style>
 <!-- Title Bar -->
 <div class="pbmit-title-bar-wrapper">
     <div class="container">
@@ -14,7 +36,7 @@ $banner = !empty($page_data->page_banner)
             <div class="pbmit-title-bar-content-inner">
                 <div class="pbmit-tbar">
                     <div class="pbmit-tbar-inner container">
-                        <h1 class="pbmit-tbar-title"> About Us</h1>
+                        <h1 class="pbmit-tbar-title"> Panel Calculator</h1>
                     </div>
                 </div>
                 <div class="pbmit-breadcrumb">
@@ -25,7 +47,7 @@ $banner = !empty($page_data->page_banner)
                         <span class="sep">
                             <i class="pbmit-base-icon-angle-right"></i>
                         </span>
-                        <span><span class="post-root post post-post current-item"> About Us</span></span>
+                        <span><span class="post-root post post-post current-item"> Panel Calculator:</span></span>
                     </div>
                 </div>
             </div>
@@ -36,138 +58,136 @@ $banner = !empty($page_data->page_banner)
 <!-- Page Content -->
 <div class="page-content about-us">
     <div class="container">
-        <form action="/calculate" method="post">
-            @csrf
-            <div class="row">
-                <h3>Panel Calculator:</h3>
-            </div>
-            <div class="row">
-                <div class="card">
-                    <div class="row">
-                        <div class="col">
-                            <label for="">
-                                select unit:
-                            </label>
-                            <div class="card-body">
 
-                                <div class="form-input">
-                                    <select name="unit" id="unit">
-                                        <option value="0">Select Unit</option>
-                                        <option value="1">mm</option>
-                                        <option value="2">cm</option>
-                                        <option value="3">m</option>
-                                        <option value="4">ft</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
 
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="card">
-                    <div class="row">
-                        <div class="col">
-                            <label for="">
-                                Enter wall Height:
-                            </label>
-                            <div class="card-body">
-                                <div class="form-input">
-                                    <input type="text" name="wall_height" value="{{$wall_height??''}}" id="">
+        <div class="container d-flex justify-content-center align-items-center min-vh-100">
+
+            <div class="col-lg-8">
+                <form action="/calculate" class="contact-form" method="post">
+                    @csrf
+
+                    <div class="row" style="margin: 20px;">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">
+                                        select unit:
+                                    </label>
+                                    <div class="card-body">
+
+                                        <div class="form-input">
+                                            <select name="unit" class="form-select text-light" id="unit" style="background-color: grey;">
+                                                <option value="0">Select Unit</option>
+                                                <option value="1">mm</option>
+                                                <option value="2">cm</option>
+                                                <option value="3">m</option>
+                                                <option value="4">ft</option>
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <label for="">
-                                Enter wall width:
-                            </label>
-                            <div class="card-body">
-                                <div class="form-input">
-                                    <input type="text" name="wall_width" value="{{$wall_width??''}}" id="">
-                                </div>
+
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="card">
-                    <div class="row">
-                        <div class="col">
-                            <label for="">
-                                select panel dimensions:
-                            </label>
-                            <div class="card-body">
-
-                                <div class="form-input">
-                                    <select name="panel_dim" id="panel_dim">
-                                        <option value="0">Select Panel Dimension</option>
-                                        <option value="1">2600mm x 250mm</option>
-                                        <option value="2">2420mm x 600mm</option>
-                                    </select>
+                    <div class="row" style="margin: 20px;">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">
+                                        Enter wall Height:
+                                    </label>
+                                    <div class="card-body">
+                                        <div class="form-input">
+                                            <input type="text" class="form-control text-light" name="wall_height" value="{{$wall_height??''}}" id="" style="background-color: grey;">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-            <div class="row" id="panel_dim_row" style="display: none;">
-                <div class="card">
-                    <div class="row">
-                        <div class="col">
-                            <label for="">
-                                Panel Height:
-                            </label>
-                            <div class="card-body">
-                                <div class="form-input">
-                                    <input type="text" name="panel_height" value="{{$panel_height??''}}" id="panel_height">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <label for="">
-                                Panel width:
-                            </label>
-                            <div class="card-body">
-                                <div class="form-input">
-                                    <input type="text" name="panel_width" value="{{$panel_width??''}}" id="panel_width">
+                                <div class="col">
+                                    <label for="">
+                                        Enter wall width:
+                                    </label>
+                                    <div class="card-body">
+                                        <div class="form-input">
+                                            <input type="text" class="form-control text-light" style="background-color: grey;" name="wall_width" value="{{$wall_width??''}}" id="">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="form-input">
-                    <input type="submit" value="Submit">
-                </div>
-            </div>
-        </form>
+                    <div class="row" style="margin: 20px;">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">
+                                        select panel dimensions:
+                                    </label>
+                                    <div class="card-body">
 
-        <div class="row justify-content-center">
-            <div class="container">
-                <div class="table">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Panel Area</th>
-                                <th>Wall Area</th>
-                                <th>Total Panel Required</th>
-                                <th>Excess Area</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>{{$panel_area??0}}</td>
-                                <td>{{$wall_area??0}}</td>
-                                <td>{{$total_panel_required??0}}</td>
-                                <td>{{$excess_area??0}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                                        <div class="form-input">
+                                            <select name="panel_dim" class="form-select text-light" style="background-color: grey;" id="panel_dim">
+                                                <option value="0">Select Panel Dimension</option>
+                                                <option value="1">2600mm x 250mm</option>
+                                                <option value="2">2420mm x 600mm</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row" id="panel_dim_row" style="margin:20px;display: none;">
+                        <div class="card">
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">
+                                        Panel Height:
+                                    </label>
+                                    <div class="card-body">
+                                        <div class="form-input">
+                                            <input type="text" name="panel_height" class="form-control text-light" style="background-color: grey;" value="{{$panel_height??''}}" id="panel_height">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <label for="">
+                                        Panel width:
+                                    </label>
+                                    <div class="card-body">
+                                        <div class="form-input">
+                                            <input type="text" name="panel_width" class="form-control text-light" style="background-color: grey;" value="{{$panel_width??''}}" id="panel_width">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row" style="margin: 20px;">
+                        <div class="form-input">
+                            <input type="submit" class="btn btn-secondary" value="Submit">
+                        </div>
+                    </div>
+                </form>
+                <table class="table table-bordered table-striped text-center shadow-lg">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Panel Area (㎡)</th>
+                            <th>Wall Area (㎡)</th>
+                            <th>Panels Required</th>
+                            <th>Excess Area (㎡)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{$panel_area ?? 0}}</td>
+                            <td>{{$wall_area ?? 0}}</td>
+                            <td>{{$total_panel_required ?? 0}}</td>
+                            <td>{{$excess_area ?? 0}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
