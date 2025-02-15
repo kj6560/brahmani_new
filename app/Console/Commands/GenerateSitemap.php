741cf0a22a00c2ff94eb6e91fd5db21502172a45
kj6560 <kj6560@gmail.com>
2025-02-16 00:44:16 +0530

    major changes

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+    protected $description = 'Generate multiple sitemaps';
+
+    public function handle()
+    {
+        // Generate main pages sitemap
+        Sitemap::create()
+            ->add(Url::create(url('/')))
+            ->add(Url::create(url('/about-us')))
+            ->add(Url::create(url('/contact-us')))
+            ->writeToFile(public_path('sitemap-pages.xml'));
+
+        // Generate sitemap for products
+        $sitemapProducts = Sitemap::create();
+        $products = Product::all();
+        foreach ($products as $product) {
+            $sitemapProducts->add(Url::create(url("/product/{$product->id}")));
+        }
+        $sitemapProducts->writeToFile(public_path('sitemap-products.xml'));
+
+        // Generate sitemap index file ✅ **Corrected**
+        $baseUrl = request()->getSchemeAndHttpHost(); // Get base URL dynamically
+
+        $sitemapIndex = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <sitemap>
+        <loc>{$baseUrl}/sitemap-pages.xml</loc>
+    </sitemap>
+    <sitemap>
+        <loc>{$baseUrl}/sitemap-products.xml</loc>
+    </sitemap>
+</sitemapindex>
+XML;
+
+        file_put_contents(public_path('sitemap.xml'), $sitemapIndex);
+
+        $this->info('Sitemaps generated successfully!');
+    }
+}
