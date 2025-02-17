b6af6ffd45b88ddf0737134a34b1bda59f5429e4
kj6560 <kj6560@gmail.com>
2025-02-17 23:06:41 +0530

    a

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -50,7 +50,7 @@ $sliders = !empty($settings['page_data']) ? json_decode($settings['page_data']->
             <?php $sliderUrl = asset('storage') . "/" . $slider ?>
             <div class="swiper-slide">
                 <div class="pbmit-slider-item">
-                    <img src="{{$sliderUrl}}" class="pbmit-slider-bg img-fluid w-100" alt="Slider Image">
+                    <img src="{{$sliderUrl}}" class="img-fluid w-100" alt="Slider Image">
                     
                 </div>
             </div>
