62b49bafe4563e98757a4000df05605b36247c65
kj6560 <kj6560@gmail.com>
2025-02-19 13:11:55 +0530

    a

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -40,8 +40,10 @@ $sliders = !empty($settings['page_data']) ? json_decode($settings['page_data']->
         /* Move right button outside */
     }
 
-    .carousel img {
-        height: 100vh;
+    @media screen and (orientation: landscape) {
+        .carousel img {
+            height: 100vh;
+        }
     }
 </style>
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
@@ -51,15 +53,24 @@ $sliders = !empty($settings['page_data']) ? json_decode($settings['page_data']->
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
     <div class="carousel-inner">
-        
         @foreach ($sliders as $slider)
-        <?php $sliderUrl = asset('storage') . "/" . $slider ?>
-        
-        <div class="carousel-item">
+        <?php $sliderUrl = asset('storage') . "/" . $slider; ?>
+        <div class="carousel-item active">
             <img src="{{$sliderUrl}}" class="d-block w-100" alt="...">
         </div>
         @endforeach
+        <!-- <div class="carousel-item active">
+            <img src="https://brahmanienterprise.co.in/storage/uploads/1735544202_demo1-slide-03.jpg" class="d-block w-100" alt="...">
 
+        </div>
+        <div class="carousel-item">
+            <img src="https://brahmanienterprise.co.in/storage/uploads/1735544202_demo1-slide-03.jpg" class="d-block w-100" alt="...">
+
+        </div>
+        <div class="carousel-item">
+            <img src="https://brahmanienterprise.co.in/storage/uploads/1735544202_demo1-slide-03.jpg" class="d-block w-100" alt="...">
+
+        </div> -->
         <div class="pbmit-slider-dots-corner">
             <div class="pbmit-sticky-corner pbmit-top-right-corner">
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="" xmlns="http://www.w3.org/2000/svg"
